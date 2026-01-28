6bf9fe0cfd5c8ded506d00c44b29f81a136b9d32
Tadhani Urvish <tadhaniurvish9@gmail.com>
2026-01-28 14:46:23 +0530

    final update

--- src/app/(admin)/layout.js
+++ src/app/(admin)/layout.js
@@ -79,6 +79,7 @@ export default function AdminLayout({ children }) {
       `}
       >
         <div className="flex items-center justify-between p-6 border-b border-slate-700">
+         
           <Link href="/admin/user">
             <span className="text-xl font-black tracking-tighter text-white">
               ADMIN PANEL
